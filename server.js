2099e7570b2edd0889b9150d1cfe59f62f44d23d
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-11 01:15:57 +0530

    run server

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+// Middleware to parse JSON request bodies
+
+
+// Middleware to validate user input
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
