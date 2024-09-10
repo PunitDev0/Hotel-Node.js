f75d7bd1578d7d45cbb80f1a16aaeb0473a603ea
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-11 01:36:49 +0530

    erore solve

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 import mongoose from 'mongoose';
 
-const mongoURL = " mongodb://127.0.0.1:27017/hotels"
+const mongoURL = "mongodb://127.0.0.1:27017/hotels";
 
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
 
-db.on('error',()=>{
-    console.log('errror');
-    
-})
-db.on('connected',()=>{
-    console.log('connected mongose');
-    
-})
-db.on('disconnected',()=>{
-    console.log('disconnected from mongose');
-})
+db.on('error', () => {
+    console.log('Error connecting to MongoDB');
+});
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+});
 
 export default db;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import db from './db.js'
 const app = express()
 
 // Middleware to parse JSON request bodies
