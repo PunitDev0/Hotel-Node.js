bf2bc2369b7708333ba94b2860bb6e912fd74a41
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-14 15:25:21 +0530

    create menu item

---
+++ Models/MenuItem.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from 'mongoose';
+
+const menuItemSchema = new  mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    price: {
+        type : Number,
+        required : true
+    },
+    is_drinks: {
+        type : Boolean,
+        required : true
+    },
+    ingredients: {
+        type : [String],
+        required : true
+    },
+    num_sales: {
+        type : Number,
+        default : 0
+    }
+
+})
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema)
+
+export default MenuItem;
