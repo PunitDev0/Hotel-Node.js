6c64493b785c9d226d6c7c5beacfebbf0949f71c
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-22 19:42:42 +0530

    Add vercel

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+const app = express();
+import db from '../db.js'
+import dotenv from 'dotenv'
+import passport from '../auth.js'
+import bodyParser from 'body-parser'
+import Person from '../Models/Person.js'
+import { VercelRequest, VercelResponse } from '@vercel/node';
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+app.use(express.json()); // Instead of bodyParser.json()
+
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
+
+app.get('/api', function (req, res) {
+    res.send('Welcome to our Hotel');
+})
+
+// Import the router files
+import personRoutes from './routes/PersonRoutes.js';
+import menuItemRoutes from './routes/MenuRoutes.js';
+
+
+// Use the routers
+app.use('/person', personRoutes);
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}` );
+})
